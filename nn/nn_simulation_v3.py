d093b7801798c9c27852c921a2ecff753cfacf21
Schrabbdian <rtbrand@online.de>
2020-04-02 16:38:24 +0200

    refactored project so that less code is duplicated

--- nn/nn_simulation_v3.py
+++ nn/nn_simulation_v3.py
@@ -1,8 +1,6 @@
 # coding=utf-8
 import matplotlib
-
-from phi.tf.flow import *
-from phi.math import upsample2x
+from nn_architecture import *
 
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
@@ -18,118 +16,12 @@ Left: Simulation using NN as solver         Right: Simulation using numeric solv
 np.random.seed(2020)  # fix seed
 perm = np.random.permutation(3000)
 
-def pressure_unet(divergence, scope="pressure_unet"):
-    """ Network structure (Based on U-Net) """
-    with tf.variable_scope(scope):
-        x = divergence
-
-        print(x.shape)
-
-        # DownConv Level 1
-        c1 = tf.layers.conv2d(x, 4, 5, strides=1, activation=tf.nn.relu, padding="same", name="conv1", trainable=True,
-                              reuse=tf.AUTO_REUSE)
-        c2 = tf.layers.conv2d(c1, 4, 5, strides=1, activation=tf.nn.relu, padding="same", name="conv2", trainable=True,
-                              reuse=tf.AUTO_REUSE)
-
-        c3 = tf.layers.conv2d(c2, 4, 5, strides=2, activation=tf.nn.relu, padding="same", name="conv3", trainable=True,
-                              reuse=tf.AUTO_REUSE)
-
-        print(c3.shape)
-
-        # DownConv Level 2
-        c4 = tf.layers.conv2d(c3, 8, 5, strides=1, activation=tf.nn.relu, padding="same", name="conv4", trainable=True,
-                              reuse=tf.AUTO_REUSE)
-        c5 = tf.layers.conv2d(c4, 8, 5, strides=1, activation=tf.nn.relu, padding="same", name="conv5", trainable=True,
-                              reuse=tf.AUTO_REUSE)
-        c6 = tf.layers.conv2d(c5, 8, 5, strides=2, activation=tf.nn.relu, padding="same", name="conv6", trainable=True,
-                              reuse=tf.AUTO_REUSE)
-        print(c6.shape)
-
-        # DownConv Level 3
-        c7 = tf.layers.conv2d(c6, 16, 5, strides=1, activation=tf.nn.relu, padding="same", name="conv7", trainable=True,
-                              reuse=tf.AUTO_REUSE)
-        c8 = tf.layers.conv2d(c7, 16, 5, strides=1, activation=tf.nn.relu, padding="same", name="conv8", trainable=True,
-                              reuse=tf.AUTO_REUSE)
-        c9 = tf.layers.conv2d(c8, 16, 5, strides=2, activation=tf.nn.relu, padding="same", name="conv9", trainable=True,
-                              reuse=tf.AUTO_REUSE)
-
-        print(c9.shape)
-
-        # Lowest Convolutions
-        c10 = tf.layers.conv2d(c9, 32, 5, strides=1, activation=tf.nn.relu, padding="same", name="conv10", trainable=True,
-                              reuse=tf.AUTO_REUSE)
-        c11 = tf.layers.conv2d(c10, 32, 5, strides=1, activation=tf.nn.relu, padding="same", name="conv11", trainable=True,
-                              reuse=tf.AUTO_REUSE)
-        c12 = tf.layers.conv2d(c11, 32, 5, strides=1, activation=tf.nn.relu, padding="same", name="conv12", trainable=True,
-                              reuse=tf.AUTO_REUSE)
-
-        print(c12.shape)
-
-        # UpConv Level 3
-        u1 = upsample2x(c12)
-        uc1 = tf.layers.conv2d(tf.concat([u1, c8], 3), 16, 5, strides=1, activation=tf.nn.relu, padding="same",
-                               name="upconv1", trainable=True, reuse=tf.AUTO_REUSE)
-        uc2 = tf.layers.conv2d(uc1, 16, 5, strides=1, activation=tf.nn.relu, padding="same", name="upconv2",
-                               trainable=True, reuse=tf.AUTO_REUSE)
-        uc3 = tf.layers.conv2d(uc2, 16, 5, strides=1, activation=tf.nn.relu, padding="same", name="upconv3",
-                               trainable=True, reuse=tf.AUTO_REUSE)
-
-        print(uc3.shape)
-
-        # UpConv Level 2
-        u2 = upsample2x(uc3)
-        uc4 = tf.layers.conv2d(tf.concat([u2, c5], 3), 8, 5, strides=1, activation=tf.nn.relu, padding="same",
-                               name="upconv4", trainable=True, reuse=tf.AUTO_REUSE)
-        uc5 = tf.layers.conv2d(uc4, 8, 5, strides=1, activation=tf.nn.relu, padding="same", name="upconv5",
-                               trainable=True, reuse=tf.AUTO_REUSE)
-        uc6 = tf.layers.conv2d(uc5, 8, 5, strides=1, activation=tf.nn.relu, padding="same", name="upconv6",
-                               trainable=True, reuse=tf.AUTO_REUSE)
-
-        print(uc6.shape)
-
-        # UpConv Level 1
-        u3 = upsample2x(uc6)
-        uc7 = tf.layers.conv2d(tf.concat([u3, c2], 3), 4, 5, strides=1, activation=tf.nn.relu, padding="same",
-                               name="upconv7", trainable=True, reuse=tf.AUTO_REUSE)
-        uc8 = tf.layers.conv2d(uc7, 4, 5, strides=1, activation=tf.nn.relu, padding="same", name="upconv8",
-                               trainable=True, reuse=tf.AUTO_REUSE)
-        uc9 = tf.layers.conv2d(uc8, 4, 5, strides=1, activation=tf.nn.relu, padding="same", name="upconv9",
-                               trainable=True, reuse=tf.AUTO_REUSE)
-
-        print(uc9.shape)
-
-        # final Convolution
-        out = tf.layers.conv2d(uc9, 1, 5, strides=1, activation=None, padding="same", name="out_conv", trainable=True,
-                               reuse=tf.AUTO_REUSE)
-
-        return out
-
-
 def random_density(shape):
     return math.maximum(0, math.randfreq(shape, power=32))
 
 
 def random_velocity(shape):
     return math.randfreq(shape, power=32) * 2
-
-# Solver that only solves up to X iterations
-def it_solver(X, acc=1e-3):
-    return SparseCG(autodiff=True, max_iterations=X, accuracy=acc)
-
-# Predict pressure using Neural Network
-def predict_pressure(divergence, normalize=True):
-
-    if normalize:
-        #divide input by its standard deviation (normalize)
-        s = math.std(divergence.data, axis=(1, 2, 3))
-        s = math.reshape(s, (-1, 1, 1, 1)) # reshape to broadcast correctly across batch
-
-        #multiply output by same factor (de-normalize)
-        result =  s * pressure_unet(divergence.data / s)
-    else:
-        result = pressure_unet(divergence.data)
-
-    return CenteredGrid(result, divergence.box, name='pressure')
 
 
 class NNPoissonSolver(PoissonSolver):
@@ -142,17 +34,8 @@ class NNPoissonSolver(PoissonSolver):
     def solve(self, field, domain, guess):
         with tf.variable_scope('model'):
 
-            if self.normalizeInput:
-                # divide input by its standard deviation (normalize)
-                s = math.std(field, axis=(1, 2, 3))
-                s = math.reshape(s, (-1, 1, 1, 1))  # reshape to broadcast correctly across batch
-
-                # multiply output by same factor (de-normalize)
-                result = s * pressure_unet(field / s)
-            else:
-                result = pressure_unet(field)
-
-            return result, None
+            result = predict_pressure(CenteredGrid(field), self.normalizeInput)
+            return result.data, None
 
 
 class NetworkSimulation(App):
@@ -163,7 +46,7 @@ class NetworkSimulation(App):
         # --- Set up Numerical Fluid Simulation ---
         self.solver = SparseCG(autodiff=True, max_iterations=500, accuracy=1e-3)
         self.smoke = world.add(Fluid(DOMAIN, name='smoke', density=random_density, velocity=random_velocity, buoyancy_factor=0.1), physics=IncompressibleFlow(pressure_solver=self.solver))
-        self.smoke_nn = world.add(Fluid(DOMAIN, name='NN_smoke', density=self.smoke.density, velocity=self.smoke.velocity, buoyancy_factor=0.1), physics=IncompressibleFlow(pressure_solver=NNPoissonSolver(normalizeInput=True)))
+        self.smoke_nn = world.add(Fluid(DOMAIN, name='NN_smoke', density=self.smoke.density, velocity=self.smoke.velocity, buoyancy_factor=0.1), physics=IncompressibleFlow(pressure_solver=NNPoissonSolver()))
 
 
         #placeholders
@@ -300,7 +183,7 @@ class NetworkSimulation(App):
             plt.savefig(path)
             plt.close()
 
-        self.info('Saved Pressure Guess Image to %s' % path)
+        self.info('Saved Pressure Guess images to %s' % path)
 
     #Use matplotlib to make diagram of residuum mean/max vs. iterations with and without guess
     def action_plot_iterations(self):
--- nn/nn_tompson_v3.py
+++ nn/nn_tompson_v3.py
@@ -1,15 +1,10 @@
 # coding=utf-8
-from phi.tf.flow import *
-from phi.math import upsample2x
-
+from nn_architecture import *
 import sys
 
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
 
 DOMAIN = Domain([64, 64], boundaries=CLOSED)  # [y, x]
-DATAPATH = 'data/smoke_v3_highaccuracy/'  # has to match DOMAIN
+DATAPATH = 'data/smoke_v3_highAccuracy/'  # has to match DOMAIN
 DESCRIPTION = u"""
 Train a neural network to predict the pressure corresponding to the given divergence field.
 The predicted pressure should be able to be fed into a solver, reducing the iterations it needs to converge.
@@ -17,111 +12,6 @@ The predicted pressure should be able to be fed into a solver, reducing the iter
 This version recreates the Tompson paper approach by using the predicted pressure to correct the velocity,
 then calculating its divergence as the loss term.
 """
-
-# Network structure (Based on U-Net)
-def pressure_unet(divergence, scope="pressure_unet"):
-    with tf.variable_scope(scope):
-        x = divergence
-
-        print(x.shape)
-
-        # DownConv Level 1
-        c1 = tf.layers.conv2d(x, 4, 5, strides=1, activation=tf.nn.relu, padding="same", name="conv1", trainable=True,
-                              reuse=tf.AUTO_REUSE)
-        c2 = tf.layers.conv2d(c1, 4, 5, strides=1, activation=tf.nn.relu, padding="same", name="conv2", trainable=True,
-                              reuse=tf.AUTO_REUSE)
-
-        c3 = tf.layers.conv2d(c2, 4, 5, strides=2, activation=tf.nn.relu, padding="same", name="conv3", trainable=True,
-                              reuse=tf.AUTO_REUSE)
-
-        print(c3.shape)
-
-        # DownConv Level 2
-        c4 = tf.layers.conv2d(c3, 8, 5, strides=1, activation=tf.nn.relu, padding="same", name="conv4", trainable=True,
-                              reuse=tf.AUTO_REUSE)
-        c5 = tf.layers.conv2d(c4, 8, 5, strides=1, activation=tf.nn.relu, padding="same", name="conv5", trainable=True,
-                              reuse=tf.AUTO_REUSE)
-        c6 = tf.layers.conv2d(c5, 8, 5, strides=2, activation=tf.nn.relu, padding="same", name="conv6", trainable=True,
-                              reuse=tf.AUTO_REUSE)
-        print(c6.shape)
-
-        # DownConv Level 3
-        c7 = tf.layers.conv2d(c6, 16, 5, strides=1, activation=tf.nn.relu, padding="same", name="conv7", trainable=True,
-                              reuse=tf.AUTO_REUSE)
-        c8 = tf.layers.conv2d(c7, 16, 5, strides=1, activation=tf.nn.relu, padding="same", name="conv8", trainable=True,
-                              reuse=tf.AUTO_REUSE)
-        c9 = tf.layers.conv2d(c8, 16, 5, strides=2, activation=tf.nn.relu, padding="same", name="conv9", trainable=True,
-                              reuse=tf.AUTO_REUSE)
-
-        print(c9.shape)
-
-        # Lowest Convolutions
-        c10 = tf.layers.conv2d(c9, 32, 5, strides=1, activation=tf.nn.relu, padding="same", name="conv10", trainable=True,
-                              reuse=tf.AUTO_REUSE)
-        c11 = tf.layers.conv2d(c10, 32, 5, strides=1, activation=tf.nn.relu, padding="same", name="conv11", trainable=True,
-                              reuse=tf.AUTO_REUSE)
-        c12 = tf.layers.conv2d(c11, 32, 5, strides=1, activation=tf.nn.relu, padding="same", name="conv12", trainable=True,
-                              reuse=tf.AUTO_REUSE)
-
-        print(c12.shape)
-
-        # UpConv Level 3
-        u1 = upsample2x(c12)
-        uc1 = tf.layers.conv2d(tf.concat([u1, c8], 3), 16, 5, strides=1, activation=tf.nn.relu, padding="same",
-                               name="upconv1", trainable=True, reuse=tf.AUTO_REUSE)
-        uc2 = tf.layers.conv2d(uc1, 16, 5, strides=1, activation=tf.nn.relu, padding="same", name="upconv2",
-                               trainable=True, reuse=tf.AUTO_REUSE)
-        uc3 = tf.layers.conv2d(uc2, 16, 5, strides=1, activation=tf.nn.relu, padding="same", name="upconv3",
-                               trainable=True, reuse=tf.AUTO_REUSE)
-
-        print(uc3.shape)
-
-        # UpConv Level 2
-        u2 = upsample2x(uc3)
-        uc4 = tf.layers.conv2d(tf.concat([u2, c5], 3), 8, 5, strides=1, activation=tf.nn.relu, padding="same",
-                               name="upconv4", trainable=True, reuse=tf.AUTO_REUSE)
-        uc5 = tf.layers.conv2d(uc4, 8, 5, strides=1, activation=tf.nn.relu, padding="same", name="upconv5",
-                               trainable=True, reuse=tf.AUTO_REUSE)
-        uc6 = tf.layers.conv2d(uc5, 8, 5, strides=1, activation=tf.nn.relu, padding="same", name="upconv6",
-                               trainable=True, reuse=tf.AUTO_REUSE)
-
-        print(uc6.shape)
-
-        # UpConv Level 1
-        u3 = upsample2x(uc6)
-        uc7 = tf.layers.conv2d(tf.concat([u3, c2], 3), 4, 5, strides=1, activation=tf.nn.relu, padding="same",
-                               name="upconv7", trainable=True, reuse=tf.AUTO_REUSE)
-        uc8 = tf.layers.conv2d(uc7, 4, 5, strides=1, activation=tf.nn.relu, padding="same", name="upconv8",
-                               trainable=True, reuse=tf.AUTO_REUSE)
-        uc9 = tf.layers.conv2d(uc8, 4, 5, strides=1, activation=tf.nn.relu, padding="same", name="upconv9",
-                               trainable=True, reuse=tf.AUTO_REUSE)
-
-        print(uc9.shape)
-
-        # final Convolution
-        out = tf.layers.conv2d(uc9, 1, 5, strides=1, activation=None, padding="same", name="out_conv", trainable=True,
-                               reuse=tf.AUTO_REUSE)
-
-        return out
-
-# Solver that only solves up to X iterations
-def it_solver(X):
-    return SparseCG(autodiff=True, max_iterations=X, accuracy=1e-6)
-
-# Predict pressure using Neural Network
-def predict_pressure(divergence, normalize=False):
-
-    if normalize:
-        #divide input by its standard deviation (normalize)
-        s = math.std(divergence.data, axis=(1, 2, 3))
-        s = math.reshape(s, (-1, 1, 1, 1)) # reshape to broadcast correctly across batch
-
-        #multiply output by same factor (de-normalize)
-        result =  s * pressure_unet(divergence.data / s)
-    else:
-        result = pressure_unet(divergence.data)
-
-    return CenteredGrid(result, divergence.box, name='pressure')
 
 def correct(velocity, pressure):
     gradp = StaggeredGrid.gradient(pressure)
@@ -134,20 +24,18 @@ class TompsonUnet(LearningApp):
         LearningApp.__init__(self, 'Training', DESCRIPTION, learning_rate=2e-4, validation_batch_size=16, training_batch_size=32, base_dir="NN_UNet3_Basic", record_data=False)
 
         # --- placeholders ---
-        #Centered Grids
-        self.divergence_in = divergence_in = placeholder(DOMAIN.centered_shape(batch_size=None))#Network Input
+        # Centered Grids
+        self.divergence_in = divergence_in = placeholder(DOMAIN.centered_shape(batch_size=None))  # Network Input
         self.true_pressure = true_pressure = placeholder(DOMAIN.centered_shape(batch_size=None))  # Ground Truth
 
-        #Staggered Grids
+        # Staggered Grids
         staggered_shape = (None, *(DOMAIN.resolution + 1), DOMAIN.rank)
 
         v_in_data = tf.placeholder(dtype=tf.float32, shape=staggered_shape)
-        self.v_in = StaggeredGrid(v_in_data)  # Velocity corresponding to Network Input
+        self.v_in = StaggeredGrid(v_in_data)  # Velocity corresponding to Network Input (Divergence)
 
         v_true_data = tf.placeholder(dtype=tf.float32, shape=staggered_shape)
         self.v_true = StaggeredGrid(v_true_data)  # Velocity corrected, Ground Truth
-
-        self.max_it = self.editable_int("Max_Iterations", 500, (500,500))# Only used for manual plotting
 
 
         # --- Build neural network ---
@@ -162,9 +50,9 @@ class TompsonUnet(LearningApp):
 
 
             # Pressure Solves with different Guesses (max iterations as placeholder)
-            self.p_predGuess, self.iter_guess = solve_pressure(divergence_in, DOMAIN, pressure_solver=it_solver(self.max_it), guess=pred_pressure)
-            self.p_trueGuess, self.iter_true  = solve_pressure(divergence_in, DOMAIN, pressure_solver=it_solver(self.max_it), guess=true_pressure)
-            self.p_noGuess,   self.iter_zero  = solve_pressure(divergence_in, DOMAIN, pressure_solver=it_solver(self.max_it), guess=None)
+            self.p_predGuess, self.iter_guess = solve_pressure(divergence_in, DOMAIN, pressure_solver=it_solver(500), guess=pred_pressure)
+            self.p_trueGuess, self.iter_true  = solve_pressure(divergence_in, DOMAIN, pressure_solver=it_solver(500), guess=true_pressure)
+            self.p_noGuess,   self.iter_zero  = solve_pressure(divergence_in, DOMAIN, pressure_solver=it_solver(500), guess=None)
 
         # --- Tompson Loss function ---
         residuum = self.v_corrected.divergence(physical_units=False)
